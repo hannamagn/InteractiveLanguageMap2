7649495c859361a9db73f30e7daf9980034a1292
hannamagn <117651421+hannamagn@users.noreply.github.com>
2025-03-15 12:47:12 +0100

    added some language-features. Controller, entity and service!

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Language } from './language.entity';
+import { LanguagesController } from './language.controller';
+import { LanguageService } from './language.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forFeature([Language])],
+  controllers: [AppController, LanguagesController],
+  providers: [AppService, LanguageService],
+
 })
 export class AppModule {}
