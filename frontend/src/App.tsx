51e2d034b281a0672039c8c4062bceb4f56eb0b6
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2025-03-18 14:14:18 +0100

    update, changed dependencies for the earlier map name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import GlobalNav from "./components/globalComponents/globalNav/GlobalNav";
 import Map from "./components/worldMapComponents/map/Map";
 import './App.css'
