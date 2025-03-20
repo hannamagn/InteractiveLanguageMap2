224f8cd305eb28ec1563b1afa2dfab23d8435e75
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2025-03-20 14:56:04 +0100

    Created a route-directory for each page that will be used. Added ghost-code for each page and started working with them. Also deleted files that were not needed outside of frontend map
    Co-authored-by: TeeDestroyer <TeeDestroyer@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalNav from "./components/globalComponents/globalNav/GlobalNav";
-import Map from "./components/worldMapComponents/map/Map";
+import HomePage from "./pages/homePage";
+import WorldMapPage from "./pages/worldmapPage";
+import DatabasePage from "./pages/databasePage";
+import CreditsPage from "./pages/creditsPage";
 import './App.css'
 
 function App() {
@@ -8,7 +11,12 @@ function App() {
   return (
     <BrowserRouter>
       <GlobalNav />
-      <Map/>
+      <Routes>
+        <Route path="/InteractiveLanguageMap2/" element={<HomePage/>}/>
+        <Route path="/InteractiveLanguageMap2/world_map" element={<WorldMapPage/>}/>
+        <Route path="/InteractiveLanguageMap2/database" element={<DatabasePage/>}/>
+        <Route path="/InteractiveLanguageMap2/credits" element={<CreditsPage/>}/>
+      </Routes>
     </BrowserRouter>
   )
 }
---
+++ frontend/src/pages/worldmapPage.tsx
@@ -0,0 +1,17 @@
+import Map from "../components/worldMapComponents/map/Map";
+import LanguageButtonContainer from "../components/worldMapComponents/LanguageButtonContainer/LanguageButtonContainer";
+
+import './worldmapPage.css'
+
+function worldmapPage() {
+
+  return (
+    <>
+      <Map/>
+      <LanguageButtonContainer/>
+    </>
+    
+  )
+}
+
+export default worldmapPage
