a0d8fc26c37d1a671ea95fc4bb19c5443ef49c89
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2025-03-20 17:59:22 +0100

    Testing, just testing some implementations

--- frontend/src/components/worldMapComponents/LanguageButton/LanguageButton.tsx
+++ frontend/src/components/worldMapComponents/LanguageButton/LanguageButton.tsx
@@ -1,6 +1,11 @@
 import Button from '@mui/material/Button';
 import './LanguageButton.css';
 
+interface LanguageButtonProps {
+    label: string;
+    onClick: () => void; // Add onClick handler to the props
+}
+
 function LanguageButton({label}: { label: string }) {
     const handleClick = () => {
         console.log('Button clicked! and we like ' + label );
