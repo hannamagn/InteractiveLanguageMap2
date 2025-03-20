ac2222a2b96f45d6825775dd0645a7f5adde0a06
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2025-03-20 17:59:22 +0100

    Testing, just testing some implementations

--- frontend/src/components/worldMapComponents/LanguageButtonContainer/LanguageButtonContainer.tsx
+++ frontend/src/components/worldMapComponents/LanguageButtonContainer/LanguageButtonContainer.tsx
@@ -1,12 +1,12 @@
 import './LanguageButtonContainer.css';
 import LanguageButton from '../LanguageButton/LanguageButton'
 
-const languages = ['English', 'Spanish', 'French', 'German', 'Japanese'];
+const languages = ['English', 'Spanish', 'French', 'German', 'Japanese','English', 'Spanish', 'French', 'German', 'Japanese','English', 'Spanish', 'French', 'German', 'Japanese'];
 
 function LanguageButtonContainer() {
   return (
     <div>
-      <h2>Select a Language</h2>
+      <h2 className = "selecth2">Select a Language</h2>
       {/* Map through the languages array and create a button for each */}
       <div className = "container">
         {languages.map((lang, index) => (
