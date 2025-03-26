3b51d56e9443aebf543f50e7039ec881152b73ca
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2025-03-26 13:20:46 +0100

    Feature and updates, created new component for the homePage, updated the functionality for the Map component to work with worldMapHeader, updated values.

--- frontend/src/components/homeComponents/worldMap/worldMapHeader.tsx
+++ frontend/src/components/homeComponents/worldMap/worldMapHeader.tsx
@@ -1,12 +1,36 @@
 import './worldMapHeader.css';
 import Map from "../../worldMapComponents/map/Map";
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 function WorldMapHeader() {
     return (
         <>
             <div className = "worldMapHeader">
-               
-                <Map/>
+                <div className="map-overlay">
+                    <h1 className = "worldMapHeaderH1">Interactive Language Map</h1>
+                    <Link to="/InteractiveLanguageMap2/world_map" style={{ textDecoration: 'none' }}>
+                        <Button 
+                            className="worldMapHeaderButton" 
+                            sx={{
+                                backgroundColor: '#574B60',
+                                color: 'white',
+                                fontSize: '1.5rem',
+                                textTransform: 'none',
+                                border: '0.5px solid black',
+                                fontFamily: 'Roboto Condensed, sans-serif',
+                                fontWeight: 'bold',
+                                letterSpacing: '0.1rem',
+                                boxShadow: '0px 0px 3px 0px #414141',
+                                '&:hover': {
+                                    backgroundColor: '#A1A2A5',
+                                },
+                            }}>
+                            World Map
+                        </Button>
+                    </Link>
+                </div>
+                <Map disableScrollZoom={true}/>
             
             </div>
             {/* <header className = "worldMapHeader">
