9def618ed41e1b4d305bf3703328e0b65e1ff3b5
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2025-04-29 15:05:42 +0200

    small gramatical changes

--- frontend/src/pages/creditsPage.tsx
+++ frontend/src/pages/creditsPage.tsx
@@ -92,8 +92,9 @@ function creditsPage() {
                     <img src="public/img/ErikBild.png" alt="Erik" className='personImg' /> 
                         <div className="personText">
                             <h3 className = "personH3">Erik Green Blomroos</h3>
-                            <p className = "personP"> I have worked mostly with the frontend and the world map page especially.
-                                Some of the things I have done is: Creating the language selector, popup and components used in the world map page.
+                            <p className = "personP"> 
+                                I have worked mostly with the frontend and especially the world map page.
+                                Some things I have done is: create the language selector, popups, and components used in the world map page.
                                 I have also connected the API to the frontend and made sure that the data is displayed correctly. 
                                 Another thing I have worked with is the CSS and made sure that the website looks good and is responsive. 
                             </p>
