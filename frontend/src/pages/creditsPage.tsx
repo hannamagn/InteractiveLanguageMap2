b8f2395c3a09de3d18c52aec150713be2299ad2a
TeeDestroyer <111562945+TeeDestroyer@users.noreply.github.com>
2025-05-19 14:48:15 +0200

    tihi

--- frontend/src/pages/creditsPage.tsx
+++ frontend/src/pages/creditsPage.tsx
@@ -25,7 +25,7 @@ function creditsPage() {
                     </div>
 
                     <div className="person" >
-                    <img src="img/pontus_bild.png" alt="Pontus" className='personImg' /> 
+                    <img src="public/pontus_bild.PNG" alt="Pontus" className='personImg' /> 
                         <div className="personText">
                             <h3 className = "personH3">Pontus Wikström</h3>
                             <p className = "personP">
